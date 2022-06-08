1760897476028b502c426bed2a268188e41c645b
Cicero Nascimento <antoniociceero@gmail.com>
2022-06-08 11:55:56 -0300

    set storyboard

--- RoleMaker/Controller/ViewController.swift
+++ RoleMaker/Controller/ViewController.swift
@@ -9,15 +9,12 @@ import UIKit
 
 class ViewController: UITableViewController {
     let localizacoes = ["Localizaçao 1", "Localização principal", "Localizaçao 3"]
-
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return localizacoes.count
     }
-    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let celula = UITableViewCell(style: .default, reuseIdentifier: nil)
         let localizacao = localizacoes[indexPath.row]
