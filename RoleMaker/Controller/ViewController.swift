8e3fff59009fbd37331d9135f0692aa0b1711419
Cicero Nascimento <antoniociceero@gmail.com>
2022-06-09 11:39:08 -0300

    card, navigation view

--- RoleMaker/Controller/ViewController.swift
+++ RoleMaker/Controller/ViewController.swift
@@ -8,39 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
     let localizacoes = ["Localizaçao 1", "Localização principal", "Localizaçao 3"]
-    
     @IBOutlet weak var tableView: UITableView!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.dataSource = self
     }
-    
-//    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        return localizacoes.count
-//    }
-//    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//        let celula = UITableViewCell(style: .default, reuseIdentifier: nil)
-//        let localizacao = localizacoes[indexPath.row]
-//        celula.textLabel?.text = localizacao
-//        return celula
-//    }
-    
 }
 
 extension ViewController: UITableViewDataSource {
-    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return localizacoes.count
     }
-    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let celula = UITableViewCell(style: .default, reuseIdentifier: nil)
         let localizacao = localizacoes[indexPath.row]
         celula.textLabel?.text = localizacao
         return celula
     }
-    
 }
---
+++ RoleMaker/View/CardTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CardTableViewCell.swift
+//  RoleMaker
+//
+//  Created by Cicero Nascimento on 09/06/22.
+//
+
+import UIKit
+
+class CardTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
