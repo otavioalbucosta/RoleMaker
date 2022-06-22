3605225e6fb12e6cdaecde52665f70f8da7aae80
Thays Lunes <thayslunesaca@gmail.com>
2022-06-22 11:47:26 -0300

    Barrinha adicionada

--- RoleMaker/Controller/SheetViewController.swift
+++ RoleMaker/Controller/SheetViewController.swift
@@ -15,13 +15,8 @@ class SheetViewController: UIViewController {
     let sheetCard = CardsTableViewController()
     let label = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 150))
     
- //   let image = UIImage(systemName: "square.and.pencil")
-   
+    var image = UIImageView(image: UIImage(systemName: "minus"))
     
-   
-   
-    
-
 //    let yellowSquare: UIView = {
 //        var view = UIView(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
 //        view.backgroundColor = .yellow
@@ -35,13 +30,19 @@ class SheetViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor.init(red: 255 / 255, green: 180 / 255, blue: 91 / 255, alpha: 1)
+        view.addSubview(image)
         
         
         sheetCard.view.translatesAutoresizingMaskIntoConstraints = false
+        image.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(sheetCard.view)
 
         
-        
+//        image.widthAnchor.constraint(equalToConstant: 75).isActive = true
+//        image.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        image.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+//        image.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+    
         sheetCard.view.leftAnchor.constraint(equalTo: view.leftAnchor, constant:16.0).isActive = true
         sheetCard.view.topAnchor.constraint(equalTo: view.topAnchor, constant: 15.0).isActive = true
         sheetCard.view.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -16.0).isActive = true
