2104617bfbfcd604a5237c5560483f799da8fbdc
Cicero Nascimento <antoniociceero@gmail.com>
2022-06-23 14:26:35 -0300

    rascunho tela role final

--- RoleMaker/Controller/SheetViewController.swift
+++ RoleMaker/Controller/SheetViewController.swift
@@ -15,48 +15,59 @@ class SheetViewController: UIViewController {
     let sheetCard = CardsTableViewController()
     let label = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 150))
     
+    let buttonFinalRole = UIButton(frame: CGRect(x: 100, y: 100, width: 100, height: 50))
+
+//    buttonFinalRole;.backgroundColor = .systemBlue
+    
+    let nv = FinalRoleViewController()
+    @objc func buttonAction(sender: UIButton!) {
+        show(nv, sender: self)
+    }
+    
     var image = UIImageView(image: UIImage(systemName: "minus"))
-    
-//    let yellowSquare: UIView = {
-//        var view = UIView(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
-//        view.backgroundColor = .yellow
-//        return view
-//    }()
-//
-    
- 
-    
-    
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
         view.backgroundColor = UIColor.init(red: 255 / 255, green: 180 / 255, blue: 91 / 255, alpha: 1)
+        
+        
+        //MARK: config button
+        buttonFinalRole.setTitle(" Criar rota ", for: .normal)
+//        buttonFinalRole.tintColor = .sys
+        buttonFinalRole.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
+        buttonFinalRole.backgroundColor = UIColor.init(red: 21 / 255, green: 28 / 255, blue: 58 / 255, alpha: 1)
+        buttonFinalRole.layer.cornerRadius = 8
+//        buttonFinalRole.setTitleColor(.black, for: .normal)
+//        buttonFinalRole.layer.borderWidth = 1
+        
+        
+        //MARK: addviews
+        view.addSubview(sheetCard.view)
+        view.addSubview(buttonFinalRole)
         view.addSubview(image)
         
-        
+        //MARK: translateAutoresizingMask
         sheetCard.view.translatesAutoresizingMaskIntoConstraints = false
         image.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(sheetCard.view)
-
+        buttonFinalRole.translatesAutoresizingMaskIntoConstraints = false
         
-//        image.widthAnchor.constraint(equalToConstant: 75).isActive = true
-//        image.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        
+        //MARK: constraints
         image.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-//        image.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
-    
+        
+        buttonFinalRole.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -23).isActive = true
+//        buttonFinalRole.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        buttonFinalRole.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 150).isActive = true
+        
         sheetCard.view.leftAnchor.constraint(equalTo: view.leftAnchor, constant:16.0).isActive = true
-        sheetCard.view.topAnchor.constraint(equalTo: view.topAnchor, constant: 15.0).isActive = true
+        sheetCard.view.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
         sheetCard.view.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -16.0).isActive = true
         sheetCard.view.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -32.0).isActive = true
         
-//        view.addSubview(yellowSquare)
-        
-        
-//
-//        yellowSquare.center = CGPoint(x: yellowSquare.bounds.size.width/2, y: yellowSquare.bounds.size.height/2)
-    
-      
-        
-
     }
 }
+
 
