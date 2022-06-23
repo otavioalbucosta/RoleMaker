195a2ebd438d69ae8e97fa335af38c7e48d5bd75
Cicero Nascimento <antoniociceero@gmail.com>
2022-06-23 14:45:37 -0300

    ajustes tela final

---
+++ RoleMaker/View/FinalScreenSwiftUIView.swift
@@ -0,0 +1,20 @@
+//
+//  FinalScreenSwiftUIView.swift
+//  RoleMaker
+//
+//  Created by Cicero Nascimento on 23/06/22.
+//
+
+import SwiftUI
+
+struct FinalScreenSwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FinalScreenSwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        FinalScreenSwiftUIView()
+    }
+}
